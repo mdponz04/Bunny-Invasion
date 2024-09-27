3923e73e9f1439749ad0af600f6525c876eefacc
DuongDucManh <manhmanhd0402@gmail.com>
2024-09-27 10:13:56 +0700

    Update grid

--- BunnyInvasion/Assets/Scripts/Testing.cs
+++ BunnyInvasion/Assets/Scripts/Testing.cs
@@ -1,23 +1,19 @@
-using CodeMonkey.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PlayerNamespace;
-using HealthNamespace;
+using mapNamespace;
+
 
 public class Testing : MonoBehaviour
 {
-    /*[SerializeField] private Player player;
-
     private void Start()
     {
-        player.OnAttack += Player_OnAttack;
+        mapNamespace.Grid grid = new mapNamespace.Grid(20, 10, 10f);
+        
     }
-    
-    private void Player_OnAttack(object sender, Player.OnAttackEventArgs e)
-    {
-        Debug.Log("Attack from: " + e.attackEndPointPosition + " to: " + e.attackPosition);
-    }*/
+
 
     
+
+
 }
